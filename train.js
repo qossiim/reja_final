ac4d309f8c448e250f22fae5bce9a32c4bda40b5
qossiim <qosimtoirov2000@gmail.com>
2026-01-01 14:34:29 +0500

    fead: A-TASK-2

--- train.js
+++ train.js
@@ -112,3 +112,17 @@ function countLetter(letter, word) {
 
   return count;
 }
+
+// A-TAST-2
+function countDigits(str) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] >= "0" && str[i] <= "9") {
+      count++;
+    }
+  }
+
+  return count;
+}
+console.log(countDigits("ad2a54y79wet0sfgb9"));
